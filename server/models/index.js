646d8ef75b1cef7218690a182058154b12982fea
genesissiana <perezgenesisa@gmail.com>
2021-09-20 13:43:02 -0700

    added category models

--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,6 @@
-const Book = require('./Book');
+const User = require('./User');
+const Product = require('./Product');
+const Category = require('./Category');
+const Order = require('./Order');
 
-module.exports = { Book };
+module.exports = { User, Product, Category, Order };
