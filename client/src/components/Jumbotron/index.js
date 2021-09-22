fd01ed74cb263df7533a213d4a934adf57a9fa03
genesissiana <perezgenesisa@gmail.com>
2021-09-22 13:35:37 -0700

    jumbotron import

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -1,11 +1,12 @@
 import React from "react";
+import heroBanner from '../../assets/hero-banner.gif'
 
 function Jumbotron({ children }) {
   return (
     <div
       style={{ height: 560, clear: "both", paddingTop: 120, textAlign: "center" }}
     >
-      {children}
+      <img src={ heroBanner } alt="" />
     </div>
   );
 }
