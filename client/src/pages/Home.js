9d69bfffac482a22854c2b66fe125987c459eb2a
genesissiana <perezgenesisa@gmail.com>
2021-09-22 13:30:15 -0700

    hero video added

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ProductList from "../components/ProductList";
 import CategoryMenu from "../components/CategoryMenu";
-import Jumbotron from "../components/Jumbotron"
+import Jumbotron from "../components/Jumbotron";
+import Banner from "../components/Banner"
 import Cart from '../components/Cart';
 
 const Home = () => {
   return (
     <div className="container">
-      <Jumbotron />
+      <Banner />    
       <CategoryMenu />
       <ProductList />
       <Cart />
