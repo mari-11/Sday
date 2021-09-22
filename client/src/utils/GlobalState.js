dd1f038fb6ac5d54210236f3f32900998e6907b0
Reyna Ramirez <ramirezverareyna1@gmail.com>
2021-09-22 14:33:07 -0500

    stripe-workin

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,24 +1,26 @@
 import React, { createContext, useContext } from "react";
-import { useProductReducer } from './reducers'
+import { useProductReducer } from './reducers';
 
+// TO instantiate the global state object:
 const StoreContext = createContext();
 const { Provider } = StoreContext;
 
+// Use Context Object for Global State
 const StoreProvider = ({ value = [], ...props }) => {
-  const [state, dispatch] = useProductReducer({
-    products: [],
-    cart: [],
-    cartOpen: false,
-    categories: [],
-    currentCategory: ''
-  });
-// use this to confirm it works!
-console.log(state);
-  return <Provider value={[state, dispatch]} {...props} />;
-};
+    const [state, dispatch] = useProductReducer({
+      products: [],
+      cart: [],
+      cartOpen: false,
+      categories: [],
+      currentCategory: '',
+    });
+    // use this to confirm it works!
+    console.log(state);
+    return <Provider value={[state, dispatch]} {...props} />;
+  };
 
-const useStoreContext = () => {
-  return useContext(StoreContext);
-};
+  const useStoreContext = () => {
+    return useContext(StoreContext);
+  };
 
-export { StoreProvider, useStoreContext };
+  export { StoreProvider, useStoreContext };
--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -4,17 +4,17 @@ export function pluralize(name, count) {
   }
   return name + 's'
 }
-
+// STEP 1 : CREATE A PROMISE TO STORE DATA WHEN IT'S OFFLINE
 export function idbPromise(storeName, method, object) {
   return new Promise((resolve, reject) => {
-    // open connection to the database `shop-shop` with the version of 1
-    const request = window.indexedDB.open('shop-shop', 1);
+    // open connection to the database `sday` with the version of 1
+    const request = window.indexedDB.open('sday', 1);
 
     // create variables to hold reference to the database, transaction (tx), and object store
     let db, tx, store;
 
     // if version has changed (or if this is the first time using the database), run this method and create the three object stores 
-    request.onupgradeneeded = function(e) {
+    request.onupgradeneeded = function (e) {
       const db = request.result;
       // create object store for each type of data and set "primary" key index to be the `_id` of the data
       db.createObjectStore('products', { keyPath: '_id' });
@@ -23,49 +23,49 @@ export function idbPromise(storeName, method, object) {
     };
 
     // handle any errors with connecting
-    request.onerror = function(e) {
-      // on database open success
-request.onsuccess = function(e) {
-  // save a reference of the database to the `db` variable
-  db = request.result;
-  // open a transaction do whatever we pass into `storeName` (must match one of the object store names)
-  tx = db.transaction(storeName, 'readwrite');
-  // save a reference to that object store
-  store = tx.objectStore(storeName);
-
-  // if there's any errors, let us know
-  db.onerror = function(e) {
-    console.log('error', e);
-  };
-
-  switch (method) {
-    case 'put':
-      store.put(object);
-      resolve(object);
-      break;
-    case 'get':
-      const all = store.getAll();
-      all.onsuccess = function() {
-        resolve(all.result);
-      };
-      break;
-    case 'delete':
-      store.delete(object._id);
-      break;
-    default:
-      console.log('No valid method');
-      break;
-  }
-  
-
-  // when the transaction is complete, close the connection
-  tx.oncomplete = function() {
-    db.close();
-  };
-};
+    request.onerror = function (e) {
       console.log('There was an error');
     };
 
+    //ADD the actual functionality for us to make a transaction with the database
+    // on database open success
+    request.onsuccess = function (e) {
+      // save a reference of the database to the `db` variable
+      db = request.result;
+      // open a transaction do whatever we pass into `storeName` (must match one of the object store names)
+      tx = db.transaction(storeName, 'readwrite');
+      // save a reference to that object store
+      store = tx.objectStore(storeName);
+
+      // if there's any errors, let us know
+      db.onerror = function (e) {
+        console.log('error', e);
+      };
+
+      // TO CHECK which value we passed into the function as a method
+      switch (method) {
+        case 'put':
+          store.put(object);
+          resolve(object);
+          break;
+        case 'get':
+          const all = store.getAll();
+          all.onsuccess = function() {
+            resolve(all.result);
+          };
+          break;
+        case 'delete':
+          store.delete(object._id);
+          break;
+        default:
+          console.log('No valid method');
+          break;
+      }
+
+      // when the transaction is complete, close the connection
+      tx.oncomplete = function () {
+        db.close();
+      };
+    };
   });
 }
-
