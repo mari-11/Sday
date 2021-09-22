8cd608812b7ecf9ac90dadacaffedd3447f5bac3
genesissiana <perezgenesisa@gmail.com>
2021-09-22 13:05:12 -0700

    add jumbotron

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ProductList from "../components/ProductList";
 import CategoryMenu from "../components/CategoryMenu";
+import Jumbotron from "../components/Jumbotron"
 import Cart from '../components/Cart';
 
 const Home = () => {
   return (
     <div className="container">
+      <Jumbotron />
       <CategoryMenu />
       <ProductList />
       <Cart />
